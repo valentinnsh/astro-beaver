274f498d26c78f2d78caea6b34eefaf068069afc
valentinnsh <valentinnsh@gmail.com>
2019-04-21 18:19:24 +0300

    Add Relax method to solve_methods module

--- eq_sys_2/solver2.f90
+++ eq_sys_2/solver2.f90
@@ -1,4 +1,4 @@
-module extra_solver
+module solve_methods
 contains
   real, parameter :: eps = 10e-8
 
@@ -13,7 +13,7 @@ contains
        tmp_sum = 0
        forall(j = 1:n, j .ne. i) tmp_sum = tmp_sum + abs(A(i,j))
 
-       if(abs(a(i,i)) .lt. tmp_sum) then
+       if(abs(a(i,i)) < tmp_sum) then
           res = .false.
           return
        end if
@@ -24,7 +24,7 @@ contains
 
   !------------------Метод Якоби------------------------------!
   subroutine jakob_method(A, B, n, X)
-    integer :: k, i, j, n
+    integer :: i, j, n
     real ::  norm_x ! норма разности X на к-м и (к-1)-м шаге
     real, dimension(:,:) :: A
     real, dimension(:) :: B, X
@@ -48,7 +48,7 @@ contains
 
 
     ! Приближаем пока norm_x = |X(k) − X(k−1)| > eps
-    X = 1
+    X = 0
     norm_x = 1
     do while(norm_x > eps)
        norm_x = sqrt(sum((matmul(X,Z)+G-X)**2))
@@ -61,7 +61,7 @@ contains
 
   !------------------Метод Зейделя----------------------------!
   subroutine seidel_method(A,B,n,X)
-    integer :: k, i, j, n
+    integer ::  i, j, n
     real ::  norm_x ! норма разности X на к-м и (к-1)-м шаге
     real, dimension(:,:) :: A
     real, dimension(:) :: B, X
@@ -79,6 +79,7 @@ contains
     forall(i = 1:n) Q(i) = B(i)/A(i,i)
 
     norm_x = 1
+    X = 0
     do while(norm_x > eps)
        prevX = X
        do i = 1,n
@@ -88,3 +89,33 @@ contains
        norm_x = sqrt(sum((X-prevX)**2))
     end do
   end subroutine seidel_method
+
+  !------------------Метод Релаксации----------------------------!
+  subroutine relax_method(A,B,n,X)
+    integer ::  i, j, n
+    real ::  maxcoord
+    real, dimension(:,:) :: A
+    real, dimension(:) :: B, X
+    real, allocatable, dimension(:) :: Q
+    real, allocatable, dimension(:,:) ::  P
+
+    if(check_diagonal_dominance(A,n) .ne. .true.) then
+       write(*,*) "В матрице А нет диагонального преобладания"
+    end if
+
+    allocate(Q(n))
+    allocate(P(n,n))
+
+    forall(i = 1:n, j = i:n) P(i,j) = -A(i,j)/A(i,i)
+    forall(i = 1:n) Q(i) = B(i)/A(i,i)
+
+    X = 0 ! Начальое приближение !
+
+    do while(maxval(abs(Q)) > eps)
+       maxcoord = maxloc(abs(Q))
+       X(maxcoord) = x(maxcoord) + Q(maxcoord)
+
+       ! Вычисляем новые невязки !
+       forall(i = 1:n) Q(i) = Q(i) + P(i,maxcoord)*Q(maxcoord)
+    end do
+  end subroutine relax_method
