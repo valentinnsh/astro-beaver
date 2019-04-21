231c205042e317162989c15985c743c236c541d4
valentinnsh <valentinnsh@gmail.com>
2019-04-21 17:11:39 +0300

    Add Jacobys method realisation and matrix diagonal dominace check

--- eq_sys_2/solver2.f90
+++ eq_sys_2/solver2.f90
@@ -1,1 +1,60 @@
-  
+module extra_solver
+contains
+  real, parameter :: eps = 10e-8
+
+  !Функция, проверяющая диагональное преобладание в матрице
+  logical function check_diagonal_dominance(A, n) result(res)
+    real, dimension(:,:) :: A
+    real :: tmp_sum
+    logical ::  res
+    integer :: n, i, j
+
+    do i = 1,n
+       tmp_sum = 0
+       forall(j = 1:n, j .ne. i) tmp_sum = tmp_sum + abs(A(i,j))
+
+       if(abs(a(i,i)) .lt. tmp_sum) then
+          res = .false.
+          return
+       end if
+    end do
+    res = .true.
+    return
+  end function check_diagonal_dominance
+
+  !------------------Метод Якоби------------------------------!
+  subroutine jakob_method(A, B, n, X)
+    integer :: k, i, j, n
+    real ::  norm_x ! норма разности X на к-м и (к-1)-м шаге
+    real, dimension(:,:) :: A
+    real, dimension(:) :: B, X
+    real, allocatable, dimension(:) :: G
+    real, allocatable, dimension(:,:) ::  Z
+
+    if(check_diagonal_dominance(A,n) .ne. .true.) then
+       write(*,*) "В матрице А нет диагонального преобладания"
+    end if
+
+    allocate(Z(n))
+    allocate(G(n))
+
+
+    ! Посчитаем матрицу Z = D^(-1)*(D − A)
+    Z = 0
+    forall(i = 1:n, j = 1:n, i .ne. j) Z(i,j) = -A(i,j)/A(j,j)
+
+    !Посчитаем теперь ыектор G
+    forall(i = 1:n) G(i) = B(i)/A(i,i)
+
+
+    ! Приближаем пока norm_x = |X(k) − X(k−1)| > eps
+    X = 1
+    norm_x = 1
+    do while(norm_x > eps)
+       norm_x = sqrt(sum((matmul(X,Z)+G-X)**2))
+       X = matmul(X,Z)+G
+    end do
+
+    deallocate(G)
+    deallocate(Z)
+  end subroutine jakob_method
