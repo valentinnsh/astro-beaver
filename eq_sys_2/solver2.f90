8c77172263a90ab4c5ab2e162951602da277f6f3
valentinnsh <valentinnsh@gmail.com>
2019-04-21 17:55:48 +0300

    Add Seidel method to solver2.f90

--- eq_sys_2/solver2.f90
+++ eq_sys_2/solver2.f90
@@ -58,3 +58,33 @@ contains
     deallocate(G)
     deallocate(Z)
   end subroutine jakob_method
+
+  !------------------Метод Зейделя----------------------------!
+  subroutine seidel_method(A,B,n,X)
+    integer :: k, i, j, n
+    real ::  norm_x ! норма разности X на к-м и (к-1)-м шаге
+    real, dimension(:,:) :: A
+    real, dimension(:) :: B, X
+    real, allocatable, dimension(:) :: Q, prevX
+    real, allocatable, dimension(:,:) ::  P
+
+    if(check_diagonal_dominance(A,n) .ne. .true.) then
+       write(*,*) "В матрице А нет диагонального преобладания"
+    end if
+    allocate(Q(n))
+    allocate(P(n,n))
+    allocate(prevX(n))
+
+    forall(i = 1:n, j = i:n) P(i,j) = -A(i,j)/A(i,i)
+    forall(i = 1:n) Q(i) = B(i)/A(i,i)
+
+    norm_x = 1
+    do while(norm_x > eps)
+       prevX = X
+       do i = 1,n
+          X(i) = Q(i) + sum(X(1:i-1)*P(i,1:i-1)) + sum(X(i+1:n)*P(i,i+1:n))
+       end do
+
+       norm_x = sqrt(sum((X-prevX)**2))
+    end do
+  end subroutine seidel_method
