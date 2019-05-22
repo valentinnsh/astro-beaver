c41811cbfc2f7337acd0eee1ca9b996f233449a8
valentinnsh <valentinnsh@gmail.com>
2019-05-22 17:20:55 +0300

    Initial commit for newton method

---
+++ newton/newton_method.f90
@@ -0,0 +1,10 @@
+!
+! Copyright (c) 2019 V.Shishkin
+!
+
+module newton_method
+  implicit none
+
+contains
+
+end module newton_method
---
+++ newton/tester.f90
@@ -0,0 +1,9 @@
+!
+! Copyright (c) 2019 V.Shishkin
+!
+
+
+program DEFAULT_NAME
+  implicit none
+
+end program DEFAULT_NAME
